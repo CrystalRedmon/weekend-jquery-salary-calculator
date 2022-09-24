aaf1aa19200ef3f73022c73308477e312635dede
Crystal Redmon <cato2848@gmail.com>
2022-09-24 09:27:29 -0500

    created and sourced in scripts.js file. Sourced in jQuery file. Created onReady function

---
+++ scripts/scripts.js
@@ -0,0 +1,17 @@
+console.log('Ready to Roll');
+
+$(document).ready(onReady);
+
+////      STATE     ///////  Initiate global variables here
+
+
+
+
+////      EVENTS    ///////  All of the events belong in onReady function
+function onReady(){
+
+
+
+
+
+}
